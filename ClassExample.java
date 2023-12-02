96ed66297b6971f6ee6959493c912704b9ae2fbd
e4e6 <hangyeol460@gmail.com>
2023-12-02 21:06:57 +0900

    add: Class, method [예제 5-3]

---
+++ ClassExample.java
@@ -0,0 +1,28 @@
+class Circle {
+    final static double PI = 3.141592;
+    double radius;
+
+    Circle(){
+        this.radius = 1;
+    }
+    Circle(double radius){
+        this.radius = radius;
+    }
+    double getArea(){
+        return radius*radius*PI;
+    }
+    double getCircumference(){
+        return radius*2*PI;
+    }
+    void setRadius(double radius){
+        this.radius = radius;
+    }
+}
+
+public class ClassExample {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        circle.setRadius(3);
+        System.out.println(circle.getArea());
+    }
+}
